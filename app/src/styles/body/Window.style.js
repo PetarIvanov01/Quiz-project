67b8f5d6a1eb3cc8889624d5d56be56159b45984
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-16 15:39:24 +0300

    Container styles

---
+++ app/src/styles/body/Window.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const WindowRoot = styled.div`
+    
+    background-color: #F1FEA3;
+    display: flex;
+    flex-grow: 1;
+`;
+
+export default WindowRoot;
