0c18977f4ef128ff5ad07d4093b7b851f8534ab6
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-16 15:39:08 +0300

    DropDownMenu styles

---
+++ app/src/styles/Header/Dropdown/DropdownMenu.style.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const DropDown = styled.nav`
+
+    margin: 0 auto;
+    margin-top: 2em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 4em;
+    height: 40%;
+    width: 3em;
+
+    &.active {
+        background-color: #D9EBD0;
+        padding: 0 2em;
+        border-radius: 1em;
+        box-shadow: 0 2px 15px 0 rgba(0,0,0,.3);
+        transition:  150ms ease-in-out;
+    }
+
+
+    .logo-mini {
+        width: 40px;
+    }
+
+`
+const DropDownMenu = styled.div`
+
+        list-style-type: none;
+        display: ${({ $isOpen }) => ($isOpen ? "flex" : "none")};
+        flex-direction: column;
+        gap: 2em;
+        align-items: center;
+        position: absolute;
+        top: 15%;
+        
+`
+
+const Menu = styled.div`
+
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+    cursor: pointer;
+
+    .logo {
+        width: 70px;
+    }
+
+`
+
+export { DropDownMenu, Menu, DropDown }
