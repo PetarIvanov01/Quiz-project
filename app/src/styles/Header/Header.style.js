ea6d2bce0e45ca1733a02029b1121b537aa4f534
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-16 15:38:50 +0300

    Heade, Main styles

---
+++ app/src/styles/Header/Header.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderRoot = styled.div`
+
+
+    background-color: #D0DA95;
+    min-width: 100px;
+
+
+`;
---
+++ app/src/styles/Main/Main.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const MainStyle = styled.div`
+
+    flex-grow: 1;
+
+.logo {
+    width: max-content;
+    margin: 0 auto;
+    margin-top: 2em;
+}
+`
+
+export default MainStyle
