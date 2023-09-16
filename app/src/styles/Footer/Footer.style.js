584d3b16fb100235c45eddef8fab79707ea41c41
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-16 15:38:40 +0300

    footer styles

---
+++ app/src/styles/Footer/Footer.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FooterRoot = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 80px;
+    background-color: #D0DA95;
+`;
+
+
+export default FooterRoot;
