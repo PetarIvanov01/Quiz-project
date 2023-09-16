3bc8e98c6b48cc5a6b57ad4e5cb48a76a6e2fe17
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-16 16:39:02 +0300

    Footer styles

---
+++ app/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import FooterRoot from "../../styles/Footer/Footer.style";
+
+export default function Footer() {
+
+
+  return (
+    <FooterRoot >
+      <p>&copy; 2023 Quiz App. All Rights Reserved.</p>
+    </FooterRoot>
+  )
+
+}
--- app/src/styles/Footer/Footer.style.js
+++ app/src/styles/Footer/Footer.style.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
 const FooterRoot = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
     justify-content: center;
     height: 80px;
     background-color: #D0DA95;
+
+    p {
+        max-width: max-content;
+        align-self: flex-start;
+        margin-left: 6em;
+    }
 `;
 
 
