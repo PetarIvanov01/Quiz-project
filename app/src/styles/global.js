bf5c41c2c92ec399999f627068b6c0fa8eb66314
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-16 15:38:28 +0300

    Global styles

---
+++ app/src/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    #root {
+        min-height: 100vh;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        background-color: wheat;
+}
+:root {
+
+/* fonts */
+--font-inter: Inter;
+--font-lemon: Lemon;
+
+/* font sizes */
+--font-size-xl: 20px;
+
+/* Colors */
+--color-black: #000;
+--color-palegoldenrod: #d0da95;
+
+/* Border radiuses */
+--br-11xl: 30px;
+
+}
+`;
+
+export default GlobalStyle
