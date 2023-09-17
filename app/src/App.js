7ed6ae30b733cad6271fcff5788c31ce22067103
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-17 12:43:25 +0300

    Update: Expanded the App component to include routing and navigation using react-router-dom.

--- app/src/App.js
+++ app/src/App.js
@@ -2,16 +2,24 @@ import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
 import WindowRoot from "./styles/body/Window.style"
+import { Route, Routes } from 'react-router-dom'
+import GamePlay from "./components/View/Game"
+import { NavigationProvider } from "./context/navigation"
 
 export default function App() {
 
   return (
     <>
       <WindowRoot>
-        <Header/>
-        <Main/>
+        <NavigationProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/game" element={<GamePlay />} />
+          </Routes>
+        </NavigationProvider>
       </WindowRoot>
-      <Footer/>
+      <Footer />
     </>
   )
 }
