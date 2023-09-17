55a0cdee99b01a46f71c4785180d08d5b7ce6bbf
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-17 12:46:51 +0300

    Restructured the Main component to improve code  organization and readability.

--- app/src/components/Main/Main.js
+++ app/src/components/Main/Main.js
@@ -1,68 +1,16 @@
-import { CategoriesSection, GameCard, GameCategory, GameQuestions,GamePlayButton } from "../../styles/Main/Categories.style";
-import { MainStyle, Logo } from "../../styles/Main/Main.style";
-import { Link } from "react-router-dom"
+import { MainStyle } from "../../styles/Main/Main.style";
+import LogoText from '../Main/Logo'
+import GameSection from "./Games/GameSection";
+
 export default function Main() {
 
 
-   return <MainStyle>
-      <Logo >
-         <Link><img src="./home-imgs/logo-home@3x.png" alt="" /></Link>
-         <div className="greating">
-            <p>Prepare to be quizzed! 🤓 Are you up for the challenge?</p>
-         </div>
-      </Logo>
+   return (
+      <MainStyle>
+         <LogoText />
 
-      <CategoriesSection>
-         <p className="categories-text">Categories</p>
-
-         <GameCard>
-            <GameCategory >
-               <img className='img-category' src="/home-imgs/categorysport1icon@3x.png" alt="" />
-               <p className="text-category">Sport</p>
-            </GameCategory>
-
-            <GameQuestions>
-               <img src="/home-imgs/questionicon1@3x.png" alt="" />
-               <p>0/5</p>
-            </GameQuestions>
-
-            <GamePlayButton>
-               <img src="/home-imgs/buttonplay3@3x.png" alt="" />
-            </GamePlayButton>
-
-         </GameCard>
-
-         <GameCard>
-            <GameCategory >
-               <img className='img-category' src="/home-imgs/categorysport1icon@3x.png" alt="" />
-               <p className="text-category">Sport</p>
-            </GameCategory>
-            <GameQuestions>
-               <img src="/home-imgs/questionicon1@3x.png" alt="" />
-               <p>0/5</p>
-            </GameQuestions>
-            <GamePlayButton>
-               <img src="/home-imgs/buttonplay3@3x.png" alt="" />
-            </GamePlayButton>
-
-         </GameCard>
-
-         <GameCard>
-            <GameCategory >
-               <img className='img-category' src="/home-imgs/categorysport1icon@3x.png" alt="" />
-               <p className="text-category">Sport</p>
-            </GameCategory>
-            <GameQuestions>
-               <img src="/home-imgs/questionicon1@3x.png" alt="" />
-               <p>0/5</p>
-            </GameQuestions>
-            <GamePlayButton>
-               <img src="/home-imgs/buttonplay3@3x.png" alt="" />
-            </GamePlayButton>
-         </GameCard>
-
-      </CategoriesSection>
-      
-   </MainStyle>
+         <GameSection />
+      </MainStyle>
+   )
 
 }
