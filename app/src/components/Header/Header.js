af4ca85bb4ae5073fdd6c92db902035c3b7dfbdf
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-17 12:44:47 +0300

    Enhanced the Header component with menu toggle functionality and dynamic styling based on navigation context

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,29 +1,24 @@
-import { useState } from "react";
-
 import { HeaderRoot } from "../../styles/Header/Header.style"
 import { DropDownMenu, Menu, DropDown } from "../../styles/Header/Dropdown/DropdownMenu.style"
 import { Link } from 'react-router-dom'
+import { useNavContext } from "../../context/navigation";
 
 export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
+  const { isMenuOpen, toggleMenu, isMenuDisabled } = useNavContext();
 
 
   return (
     <HeaderRoot>
-      <DropDown className={isMenuOpen ? 'active' : ''}>
+      <DropDown className={isMenuDisabled ? 'disable' : isMenuOpen ? 'active' : ''} >
 
         <Menu onClick={toggleMenu} className="menu">
           <Link><img className="logo" src="./home-imgs/menu-icon@3x.png" alt="" /></Link>
         </Menu>
 
         <DropDownMenu $isOpen={isMenuOpen} >
-          <li><Link><img className="logo-mini" src="./home-imgs/mini-home-icon@3x.png" alt="" /></Link></li>
-          <li><Link><img className="logo-mini" src="./home-imgs/throphy-icon@3x.png" alt="" /></Link></li>
+          <li><Link to={'/'}><img className="logo-mini" src="./home-imgs/mini-home-icon@3x.png" alt="" /></Link></li>
+          <li><Link to={'/leaderboard'}><img className="logo-mini" src="./home-imgs/throphy-icon@3x.png" alt="" /></Link></li>
           <li><Link><img className="logo-mini" src="./home-imgs/user-icon@3x.png" alt="" /></Link></li>
         </DropDownMenu>
 
