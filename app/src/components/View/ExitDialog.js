b1aa4665247d5d70a32a0344278648a3e71d74a9
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-20 10:35:27 +0300

    Add Exit Confirmation Dialog Component

---
+++ app/src/components/View/ExitDialog.js
@@ -0,0 +1,24 @@
+import { ExitConfirmationModal, ExitConfirmationOverlay, ModalButton, ModalText } from "../../styles/modals/ExitDialog.style";
+import { useNavigate } from 'react-router-dom'
+export default function ExitDialogComponent({ setShowExitConfirmation }) {
+
+    const navigate = useNavigate()
+
+    const handleExitConfirm = () => {
+        navigate('/')
+    }
+
+    const handleExitCancel = () => {
+        setShowExitConfirmation(false);
+    }
+
+    return (
+        <ExitConfirmationOverlay>
+            <ExitConfirmationModal>
+                <ModalText>Are you sure you want to exit the game?</ModalText>
+                <ModalButton onClick={handleExitConfirm}>Yes</ModalButton>
+                <ModalButton onClick={handleExitCancel}>No</ModalButton>
+            </ExitConfirmationModal>
+        </ExitConfirmationOverlay>
+    )
+}
