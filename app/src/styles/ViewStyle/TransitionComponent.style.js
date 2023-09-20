186e2763683edd467e486b91d21d9c9922657ede
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-20 10:36:51 +0300

    Styles for Transition count

---
+++ app/src/styles/ViewStyle/TransitionComponent.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const TransitionStyle = styled.div`
+
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`
+
+export const TimerStartStyle = styled.div`
+
+        p {
+            font-size: 10em;
+            font-family: var(--font-lemon)
+        }
+
+`
