69085a21e8f537c94eb4d3880012eb91f73c1d1b
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-22 10:49:33 +0300

    Add useEffect hook to enable navigation in Main componen

--- app/src/components/Main/Main.js
+++ app/src/components/Main/Main.js
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
+import { useNavContext } from "../../context/navigation";
 import { MainStyle } from "../../styles/Main/Main.style";
 import LogoText from '../Main/Logo'
 import GameSection from "./Games/GameSection";
 
 export default function Main() {
 
+   const { notDisabled } = useNavContext();
+   useEffect(()=>{
+
+      notDisabled()
+
+   },[notDisabled])
 
    return (
       <MainStyle>
