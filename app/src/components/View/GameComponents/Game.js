9534b1864ee68b014834fbc0c1a427b8cea5fd8a
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-22 10:50:24 +0300

    Add GamePlay component for playing the quiz game

---
+++ app/src/components/View/GameComponents/Game.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { useNavContext } from "../../../context/navigation";
+import { ExitButton, GamePlayStyle, LogoForGames, TimerStyle } from "../../../styles/ViewStyle/Game.style";
+import { Link } from "react-router-dom";
+import ExitDialogComponent from "../ExitDialog";
+import QuestionAndAnswers from "./QuestionAnswerComponent";
+
+const GAMETIME = 10
+export default function GamePlay() {
+
+    const navigate = useNavigate();
+    const { isDisabled } = useNavContext();
+    const [showExitConfirmation, setShowExitConfirmation] = useState(false);
+
+    const quizData = useOutletContext();
+    const [currentQuestion, setCurrentQuestion] = useState({});
+
+    const [timer, setTimer] = useState(GAMETIME);
+
+    useEffect(() => {
+        isDisabled();
+
+        if (timer === GAMETIME && quizData.length !== 0) {
+            setCurrentQuestion(quizData.pop());
+        }
+        else if (quizData.length === 0 && timer === 0) {
+        //Todo navigate to the end of the game
+            return navigate('/')
+        }
+
+        const countdownInterval = setInterval(() => {
+            if (!showExitConfirmation) {
+
+                if (timer > 0) {
+                    setTimer(timer - 1);
+                } else {
+                    clearInterval(countdownInterval);
+                    setTimer(GAMETIME)
+                }
+            }
+        }, 1000);
+
+        return () => clearInterval(countdownInterval);
+
+    }, [isDisabled, timer, showExitConfirmation, quizData])
+
+
+
+    const showExitDialog = () => {
+
+        setShowExitConfirmation(true);
+
+    }
+
+
+    return (
+        <GamePlayStyle>
+            <ExitButton onClick={showExitDialog} ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-83.6 290.5c4.8 4.8 4.8 12.6 0 17.4l-40.5 40.5c-4.8 4.8-12.6 4.8-17.4 0L256 313.3l-66.5 67.1c-4.8 4.8-12.6 4.8-17.4 0l-40.5-40.5c-4.8-4.8-4.8-12.6 0-17.4l67.1-66.5-67.1-66.5c-4.8-4.8-4.8-12.6 0-17.4l40.5-40.5c4.8-4.8 12.6-4.8 17.4 0l66.5 67.1 66.5-67.1c4.8-4.8 12.6-4.8 17.4 0l40.5 40.5c4.8 4.8 4.8 12.6 0 17.4L313.3 256l67.1 66.5z" /></svg></ExitButton>
+            <LogoForGames>
+
+                <Link><img src="/home-imgs/logo-home@3x.png" alt="" /></Link>
+
+            </LogoForGames>
+
+            <TimerStyle >
+                <p>{timer}</p>
+            </TimerStyle>
+
+            {currentQuestion.hasOwnProperty('question') && <QuestionAndAnswers timer={timer} quizData={currentQuestion} />}
+
+            {showExitConfirmation && <ExitDialogComponent setShowExitConfirmation={setShowExitConfirmation} />}
+
+        </GamePlayStyle>
+    )
+}
