249ce473753cdb14b84c786f38e85742c49c8b5d
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-23 17:00:47 +0300

    Update GameCard component with category images and links

--- app/src/components/Main/Games/GameCard.js
+++ app/src/components/Main/Games/GameCard.js
@@ -8,7 +8,7 @@ export default function GameCard() {
         <>
             <GameCardStyle>
                 <GameCategory >
-                    <img className='img-category' src="/home-imgs/categorysport1icon@3x.png" alt="" />
+                    <img className='img-category' src="/home-imgs/categories/sport.png" alt="sport" />
                     <p className="text-category">Sport</p>
                 </GameCategory>
 
@@ -18,37 +18,37 @@ export default function GameCard() {
                 </GameQuestions>
 
                 <GamePlayButton>
-                    <Link to={'/game'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
+                    <Link to={'/game/sport'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
                 </GamePlayButton>
 
             </GameCardStyle>
 
             <GameCardStyle>
                 <GameCategory >
-                    <img className='img-category' src="/home-imgs/categorysport1icon@3x.png" alt="" />
-                    <p className="text-category">Sport</p>
+                    <img className='img-category' src="/home-imgs/categories/history.png" alt="history" />
+                    <p className="text-category">History</p>
                 </GameCategory>
                 <GameQuestions>
                     <img src="/home-imgs/questionicon1@3x.png" alt="" />
                     <p>0/5</p>
                 </GameQuestions>
                 <GamePlayButton>
-                    <Link to={'/game'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
+                    <Link to={'/game/history'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
                 </GamePlayButton>
 
             </GameCardStyle>
 
             <GameCardStyle>
                 <GameCategory >
-                    <img className='img-category' src="/home-imgs/categorysport1icon@3x.png" alt="" />
-                    <p className="text-category">Sport</p>
+                    <img className='img-category' src="/home-imgs/categories/sport.png" alt="general" />
+                    <p className="text-category">General</p>
                 </GameCategory>
                 <GameQuestions>
                     <img src="/home-imgs/questionicon1@3x.png" alt="" />
                     <p>0/5</p>
                 </GameQuestions>
                 <GamePlayButton>
-                    <Link to={'/game'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
+                    <Link to={'/game/general'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
                 </GamePlayButton>
             </GameCardStyle>
         </>
