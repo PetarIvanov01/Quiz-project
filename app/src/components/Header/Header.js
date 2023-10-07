7ea28ddb6ec506046d70aaaa30235124e359985d
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-07 19:13:37 +0300

    Refactored the Header component to use the updated structure of the menu object from the context.  This change improves code readability and consistency.

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -5,18 +5,18 @@ import { useNavContext } from "../../context/navigation";
 
 export default function Header() {
 
-  const { isMenuOpen, toggleMenu, isMenuDisabled } = useNavContext();
+  const { menu: { isOpen, disabled }, toggleMenu } = useNavContext();
 
 
   return (
     <HeaderRoot>
-      <DropDown className={isMenuDisabled ? 'disable' : isMenuOpen ? 'active' : ''} >
+      <DropDown className={disabled ? 'disable' : isOpen ? 'active' : ''} >
 
         <Menu onClick={toggleMenu} className="menu">
           <Link><img className="logo" src="/home-imgs/menu-icon@3x.png" alt="" /></Link>
         </Menu>
 
-        <DropDownMenu $isOpen={isMenuOpen} >
+        <DropDownMenu $isOpen={isOpen} >
           <li><Link to={'/'}><img className="logo-mini" src="/home-imgs/mini-home-icon@3x.png" alt="" /></Link></li>
           <li><Link to={'/leaderboard'}><img className="logo-mini" src="/home-imgs/throphy-icon@3x.png" alt="" /></Link></li>
           <li><Link><img className="logo-mini" src="/home-imgs/user-icon@3x.png" alt="" /></Link></li>
