31167734a75cfb799bb7aa6e7c209a1c7cc7e3b4
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-10 19:09:35 +0300

    Create AnswerComponent for displaying quiz answer choices

---
+++ app/src/components/View/GameComponents/AnswerField.js
@@ -0,0 +1,23 @@
+import decodeHtmlEntities from '../../../util/decode';
+import styles from "../../../css/answer.module.css"
+import { Answer, AnswerField } from "../../../styles/ViewStyle/Game.style";
+
+
+const AnswerComponent = ({ answer,  isCorrect, onAnswerClick, index }) => {
+
+
+    return (
+
+        <AnswerField
+            className={isCorrect.name === answer.text && styles[isCorrect.answer]}
+            data-name={answer.text}
+            data-answer={answer.correct}
+            onClick={onAnswerClick}
+        >
+            <p>{String.fromCharCode(65 + index)}. </p>
+            <Answer>{decodeHtmlEntities(answer.text)}</Answer>
+        </AnswerField>
+    );
+};
+
+export default AnswerComponent;
