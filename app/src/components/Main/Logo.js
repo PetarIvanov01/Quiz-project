b108cf4088c2147e29810db10f67d87f0a64da9b
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-11 22:49:05 +0300

    Update

--- app/src/components/Main/Logo.js
+++ app/src/components/Main/Logo.js
@@ -1,16 +1,15 @@
-import { Link } from "react-router-dom"
 import { Logo } from "../../styles/Main/Main.style"
 
 export default function LogoText() {
-    
+
 
     return (
         <Logo >
-            <Link><img src="/home-imgs/logo-home@3x.png" alt="" /></Link>
+            <img src="/home-imgs/logo-home@3x.png" alt="" />
             <div className="greating">
-               <p>Prepare to be quizzed! 🤓 Are you up for the challenge?</p>
+                <p>Prepare to be quizzed! 🤓 Are you up for the challenge?</p>
             </div>
-         </Logo>
+        </Logo>
     )
 
 }
--- app/src/index.js
+++ app/src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <GlobalStyle />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode> 
 );
 
