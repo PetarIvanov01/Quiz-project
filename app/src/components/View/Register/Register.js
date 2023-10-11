d92b4e2df78075767eb0ffd8d2f3fbfa9c261424
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-11 22:46:14 +0300

    Created register component

---
+++ app/src/components/View/Register/Register.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { MainStyle } from "../../../styles/Main/Main.style";
+import { ActionLink, FormStyle, FormStyleContainer, PageLogo, TypeFieldStyle } from "../../../styles/ViewStyle/AuthForms/LoginStyle.style";
+
+export default function RegsiterView() {
+
+    return (
+
+        <MainStyle>
+
+            <FormStyleContainer>
+
+                <PageLogo>
+                    <img src="/home-imgs/nerd.png" alt="" />
+                    <p className="sign-in-greeting">Log in and prove that you're the ultimate quizmaster!</p>
+                </PageLogo>
+                
+                <FormStyle>
+
+                    <TypeFieldStyle>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" />
+                    </TypeFieldStyle>
+                    <TypeFieldStyle>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" />
+                    </TypeFieldStyle>
+                    <TypeFieldStyle>
+                        <label htmlFor="re-password">Confirm password</label>
+                        <input type="password" id="password" />
+                    </TypeFieldStyle>
+
+
+                    <ActionLink >
+                        <p>Not a quizzer yet? </p>
+                        <Link to={'/register'}>Sign up</Link>
+                    </ActionLink>
+
+                </FormStyle>
+            </FormStyleContainer>
+
+        </MainStyle>
+
+    )
+}
