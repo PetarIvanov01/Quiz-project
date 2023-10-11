4fb083c4578f6ac4fa674dbe04d1361932facf9c
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-11 22:47:19 +0300

    Addded three more routes for Leaderboard, login and register views

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,15 @@
-import Header from "./components/Header/Header"
-import Main from "./components/Main/Main"
-import Footer from "./components/Footer/Footer"
 import WindowRoot from "./styles/body/Window.style"
-import { Route, Routes } from 'react-router-dom'
 import { NavigationProvider } from "./context/navigation"
+import Header from "./components/Header/Header"
+import { Route, Routes } from 'react-router-dom'
+import Main from "./components/Main/Main"
 import TransitionToGame from "./components/View/TransitionComponent"
 import GamePlay from "./components/View/GameComponents/Game"
 import CompleteGame from "./components/View/GameFinish/CompleteGame"
+import Footer from "./components/Footer/Footer"
+import LoginView from "./components/View/Login/Login"
+import RegsiterView from "./components/View/Register/Register"
+import LeaderboardView from "./components/View/Leaderboard/LeaderboardView"
 
 export default function App() {
 
@@ -21,6 +24,9 @@ export default function App() {
               <Route path="/game/:category" element={<GamePlay />} />
             </Route>
             <Route path="/game/:category/complete" element={<CompleteGame />} />
+            <Route path="/leaderboard" element={<LeaderboardView />} />
+            <Route path="/login" element={<LoginView />} />
+            <Route path="/register" element={<RegsiterView />} />
           </Routes>
         </NavigationProvider>
       </WindowRoot>
