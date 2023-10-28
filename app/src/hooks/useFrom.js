a4693c95d54ca22c5ae6d623eac84b3e0faa5d61
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-28 19:42:38 +0300

    Implement a custom 'useForm' hook for form management

---
+++ app/src/hooks/useFrom.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+
+export function useForm(initialState, onSubmitHandler) {
+
+    const [values, setValues] = useState(initialState);
+
+    const onChangeValues = (e) => {
+        setValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        onSubmitHandler(values);
+    }
+
+
+    return { values, onChangeValues, onSubmit }
+
+}
