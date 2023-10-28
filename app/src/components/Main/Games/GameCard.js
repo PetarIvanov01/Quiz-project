cfc874de1d476a3d1bd0e14ee5c66a6ae56f16f3
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-28 19:35:47 +0300

    refactor: Simplify GameCard component by making it dynamic

--- app/src/components/Main/Games/GameCard.js
+++ app/src/components/Main/Games/GameCard.js
@@ -1,56 +1,27 @@
 import { Link } from "react-router-dom";
 import { GameCardStyle, GameCategory, GamePlayButton, GameQuestions } from "../../../styles/Main/Categories.style";
 
-export default function GameCard() {
+export default function GameCard({ imgLink, category, to }) {
+
+
 
 
     return (
-        <>
-            <GameCardStyle>
-                <GameCategory >
-                    <img className='img-category' src="/home-imgs/categories/sport.png" alt="sport" />
-                    <p className="text-category">Sport</p>
-                </GameCategory>
+        <GameCardStyle>
+            <GameCategory >
+                <img className='img-category' src={imgLink} alt={category} />
+                <p className="text-category">{category}</p>
+            </GameCategory>
 
-                <GameQuestions>
-                    <img src="/home-imgs/questionicon1@3x.png" alt="" />
-                    <p>0/5</p>
-                </GameQuestions>
+            <GameQuestions>
+                <img src="/home-imgs/questionicon1@3x.png" alt="" />
+                <p>0/5</p>
+            </GameQuestions>
 
-                <GamePlayButton>
-                    <Link to={'/game/sport'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
-                </GamePlayButton>
+            <GamePlayButton>
+                <Link to={`/game/${to}`}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
+            </GamePlayButton>
 
-            </GameCardStyle>
-
-            <GameCardStyle>
-                <GameCategory >
-                    <img className='img-category' src="/home-imgs/categories/history.png" alt="history" />
-                    <p className="text-category">History</p>
-                </GameCategory>
-                <GameQuestions>
-                    <img src="/home-imgs/questionicon1@3x.png" alt="" />
-                    <p>0/5</p>
-                </GameQuestions>
-                <GamePlayButton>
-                    <Link to={'/game/history'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
-                </GamePlayButton>
-
-            </GameCardStyle>
-
-            <GameCardStyle>
-                <GameCategory >
-                    <img className='img-category' src="/home-imgs/categories/general.png" alt="general" />
-                    <p className="text-category">General</p>
-                </GameCategory>
-                <GameQuestions>
-                    <img src="/home-imgs/questionicon1@3x.png" alt="" />
-                    <p>0/5</p>
-                </GameQuestions>
-                <GamePlayButton>
-                    <Link to={'/game/general'}><img src="/home-imgs/buttonplay3@3x.png" alt="" /></Link>
-                </GamePlayButton>
-            </GameCardStyle>
-        </>
+        </GameCardStyle>
     )
 }
