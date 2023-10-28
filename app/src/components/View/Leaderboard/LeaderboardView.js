0633d5245f6a9963994006f5ed60b54775a89b4b
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-28 19:39:49 +0300

    Update LeaderboardView with JSX elements and styles

--- app/src/components/View/Leaderboard/LeaderboardView.js
+++ app/src/components/View/Leaderboard/LeaderboardView.js
@@ -1,4 +1,21 @@
+import { LeadContainer, LeadLogo, LeadPlayersContainer, SortByContainer } from "../../../styles/ViewStyle/Leaderboard/Leaderboard.style";
+
 export default function LeaderboardView() {
-    
-    return 
+
+    return (
+        <LeadContainer>
+
+            <LeadLogo src="/home-imgs/logo-home@3x.png" alt="" />
+
+            <LeadPlayersContainer>
+                <SortByContainer>
+
+                </SortByContainer>
+
+                
+
+            </LeadPlayersContainer>
+
+        </LeadContainer>
+    )
 }
