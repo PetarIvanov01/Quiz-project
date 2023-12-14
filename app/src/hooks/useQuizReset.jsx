e10f3f3a1f88bdf24eecdca28431c0b99e5dd619
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-14 21:17:44 +0200

    refactor: Enhance useQuizReset with useCallback for optimal performance
    In the useQuizReset custom hook, optimize the onClickReset function by wrapping it with useCallback. This improves performance by memoizing the function and preventing unnecessary renders in components that utilize this hook. The behavior and functionality remain unchanged, ensuring a more efficient implementation.

--- app/src/hooks/useQuizReset.jsx
+++ app/src/hooks/useQuizReset.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 export default function useQuizReset(setTimer) {
 
     const [userAnswers, setUserAnswer] = useState([]);
 
-    const onClickReset = (currentAnswerByUser) => {
-        setTimer(0)
-        setUserAnswer(state => [...state, currentAnswerByUser])
-    }
+    const onClickReset = useCallback((currentAnswerByUser) => {
+        setTimer(0);
+        setUserAnswer(state => [...state, currentAnswerByUser]);
+    }, []);
 
-
-
-    return { onClickReset, userAnswers}
+    return { onClickReset, userAnswers };
 }
