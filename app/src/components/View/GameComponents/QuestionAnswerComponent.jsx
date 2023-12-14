23b2fb80caa0b86e3e602a47906825ed8c528a53
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-14 20:51:18 +0200

    Refactor QuestionAndAnswers component to improve state management and code readability. Replace separate state variables for isAnswered, isDisabled, and isCorrect with a single state object answerStatus. Consolidate state related to the selected answer into a single object answerData for better organization. Use useMemo to memoize the onAnswerClick callback, preventing unnecessary re-renders of the function on each render. Optimize and enhance code structure to adhere to best practices and improve maintainability.

--- app/src/components/View/GameComponents/QuestionAnswerComponent.jsx
+++ app/src/components/View/GameComponents/QuestionAnswerComponent.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { AnswerContainer, QuestionBoxStyle, SectionStyle } from "../../../styles/ViewStyle/Game.style";
 
 import decodeHtmlEntities from "../../../util/decode";
@@ -6,11 +6,12 @@ import AnswerComponent from "./AnswerField";
 
 const QuestionAndAnswers = ({ currentQuestion, onClickReset }) => {
 
-    const [isAnswered, setAnswer] = useState(false);
-    const [isDisabled, setDisable] = useState(false);
+    const [answerStatus, setAnswerStatus] = useState({
+        isAnswered: false,
+        isDisabled: false
+    });
 
-
-    const [isCorrect, setIsCorrect] = useState({
+    const [answerData, setAnswerData] = useState({
         name: '',
         answer: '',
         correct: 0,
@@ -19,42 +20,44 @@ const QuestionAndAnswers = ({ currentQuestion, onClickReset }) => {
     useEffect(() => {
 
         const timeout = setTimeout(() => {
-            setDisable(false)
+            setAnswerStatus(prev => ({ ...prev, isDisabled: false }));
 
         }, 1000)
 
-        if (isAnswered) {
-            onClickReset({ ...currentQuestion, answer: isCorrect.answer === 'correct' ? true : false });
-            setAnswer(false);
+        if (answerStatus.isAnswered) {
+            onClickReset({ ...currentQuestion, answer: answerData.answer === 'correct' ? true : false });
+            setAnswerStatus(prev => ({ ...prev, isAnswered: false }));
         }
 
         return () => clearTimeout(timeout);
 
-    }, [currentQuestion, isCorrect.answer, isAnswered, onClickReset, isDisabled])
+    }, [currentQuestion, answerData.answer, answerStatus.isAnswered, onClickReset, answerStatus.isDisabled])
 
-    const onAnswerClick = (e) => {
-        setAnswer(true);
-        setDisable(true);
 
-        const target = e.currentTarget;
-        const answer = target.getAttribute('data-answer');
-        const name = target.getAttribute('data-name');
+    const onAnswerClickMemoized = useMemo(() => {
 
-        if (answer === 'true') {
-            setIsCorrect((prev) => ({
-                name,
-                answer: 'correct',
-                correct: prev.correct += 1
-            }));
+        return (e) => {
+            setAnswerStatus(prev => ({ ...prev, isAnswered: true, isDisabled: true }));
+            const target = e.currentTarget;
+            const answer = target.getAttribute('data-answer');
+            const name = target.getAttribute('data-name');
+
+            if (answer === 'true') {
+                setAnswerData((prev) => ({
+                    name,
+                    answer: 'correct',
+                    correct: prev.correct += 1
+                }));
+            }
+            else if (answer === 'false') {
+                setAnswerData((prev) => ({
+                    name,
+                    answer: 'in-correct',
+                    correct: prev.correct
+                }));
+            }
         }
-        else if (answer === 'false') {
-            setIsCorrect((prev) => ({
-                name,
-                answer: 'in-correct',
-                correct: prev.correct
-            }));
-        }
-    }
+    }, []);
 
     return (
         <>
@@ -62,7 +65,7 @@ const QuestionAndAnswers = ({ currentQuestion, onClickReset }) => {
                 <p>{decodeHtmlEntities(currentQuestion?.question)}</p>
             </QuestionBoxStyle>
 
-            <AnswerContainer className={isDisabled ? 'disable' : ''}>
+            <AnswerContainer className={answerStatus.isDisabled ? 'disable' : ''}>
 
                 <SectionStyle  >
                     {currentQuestion?.answers.map((answer, index) => (
@@ -70,8 +73,8 @@ const QuestionAndAnswers = ({ currentQuestion, onClickReset }) => {
                             key={index}
                             index={index}
                             answer={answer}
-                            isCorrect={isCorrect}
-                            onAnswerClick={onAnswerClick}
+                            answerData={answerData}
+                            onAnswerClick={onAnswerClickMemoized}
                         />
                     ))}
                 </SectionStyle>
