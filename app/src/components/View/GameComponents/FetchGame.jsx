91f75664f33d4138062ad71b97552a86ca647915
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-14 20:44:58 +0200

    Restructure FetchQuestionData to use navigation context for disable check, switch to direct usage of quizService for API calls, and improve loading and error handling. Introduce Outlet and context for managing quizData. Enhance overall readability and maintainability.

--- app/src/components/View/GameComponents/FetchGame.jsx
+++ app/src/components/View/GameComponents/FetchGame.jsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from 'react';
+import { useNavContext } from '../../../context/navigation';
 import { Outlet } from 'react-router-dom'
-import getCategory from '../../../services/QuizService';
+
+import getCategory from "../../../services/quizService"
 import { LoadingSpinner } from '../../../styles/modals/LoadingSpinner.style';
 
 export default function FetchQuestionData({
     category
 }) {
+    const { isDisabled } = useNavContext();
 
     const [quizData, setQuizData] = useState(null);
     const [dataFetched, setDataFetched] = useState(false);
 
     useEffect(() => {
+        isDisabled();
+
+    }, [isDisabled])
+
+    useEffect(() => {
         if (category && !dataFetched) {
-            getCategory[category]()
+            const getCategoryFunc = getCategory[category];
+            getCategoryFunc()
                 .then(e => {
                     setQuizData([...e]);
                     setDataFetched(true);
