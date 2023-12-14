e1ed4f7dc9d9ac8715f40959754fd5a90a8666f6
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-14 20:57:23 +0200

    Refactor ExitDialogComponent to receive onClickExitConfirm and onClickExitCancel as props for better flexibility. Update the component to utilize these props directly, improving its reusability. Simplify and enhance code readability in line with best practices. Ensure consistency in naming and parameter usage.

--- app/src/components/View/ExitDialog.jsx
+++ app/src/components/View/ExitDialog.jsx
@@ -1,24 +1,13 @@
-import { useNavigate } from 'react-router-dom'
-
 import { ExitConfirmationModal, ExitConfirmationOverlay, ModalButton, ModalText } from "../../styles/modals/ExitDialog.style";
-export default function ExitDialogComponent({ setShowExitConfirmation }) {
 
-    const navigate = useNavigate()
-
-    const handleExitConfirm = () => {
-        navigate('/')
-    }
-
-    const handleExitCancel = () => {
-        setShowExitConfirmation(false);
-    }
+export default function ExitDialogComponent({ onClickExitConfirm, onClickExitCancel }) {
 
     return (
         <ExitConfirmationOverlay>
             <ExitConfirmationModal>
                 <ModalText>Are you sure you want to exit the game?</ModalText>
-                <ModalButton onClick={handleExitConfirm}>Yes</ModalButton>
-                <ModalButton onClick={handleExitCancel}>No</ModalButton>
+                <ModalButton onClick={onClickExitConfirm}>Yes</ModalButton>
+                <ModalButton onClick={onClickExitCancel}>No</ModalButton>
             </ExitConfirmationModal>
         </ExitConfirmationOverlay>
     )
